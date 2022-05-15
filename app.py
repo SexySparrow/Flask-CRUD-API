e7bcc6961a0c08f45c614ddbc9f7472235908b33
Adrian Brabete <30392634+SexySparrow@users.noreply.github.com>
2022-05-15 17:39:35 +0300

    Added GET by id

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def create_transport():
 
 @cross_origin()    
 @app.route('/gettransports', methods = ['GET'])
-def getpets():
+def get_transports():
      all_transports = []
      transports = Transport.query.all()
      for transport in transports:
@@ -86,6 +86,31 @@ def getpets():
                 "total_transports": len(transports),
             }
         )
+
+
+@cross_origin()    
+@app.route('/transports/<int:id>', methods = ['GET'])
+def get_transport(id):
+    transport = Transport.query.get(id)
+    if transport is None:
+        abort(404)
+    else:
+        result = {
+                  "id":transport.id,
+                  "transport_name":transport.transport_name,
+                  "transport_phone_number":transport.transport_phone_number,
+                  "transport_truck_number":transport.transport_truck_number,
+                  "transport_departure_time":transport.transport_departure_time,
+                  "transport_departure_location":transport.transport_departure_location,
+                  "transport_arrival_location":transport.transport_arrival_location,
+                  "transport_weight":transport.transport_weight,
+                  "transport_cargo_type":transport.transport_cargo_type,
+                  "transport_cargo_description":transport.transport_cargo_description }
+        return jsonify(
+                {
+                "success": True,
+                "transport": result,
+                })
 
 
 @cross_origin()  
